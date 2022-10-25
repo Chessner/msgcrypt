c65ccff9579641e58cb7089dd5115f4fd7d3e1bd
Chessner <schachnerflorian@gmail.com>
2022-10-25 15:28:28 +0200

    Seems to work

--- msgcrypt.cpp
+++ msgcrypt.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-   // #define MESSAGE (const unsigned char *) "HelloWorld"
-  //  #define MESSAGE_LEN 10
+   // #define MESSAGE (const unsigned char *) "test"
+   // #define MESSAGE_LEN 4
     #define ADDITIONAL_DATA (const unsigned char *) "123456"
     #define ADDITIONAL_DATA_LEN 6
 
@@ -49,8 +49,8 @@ int main(int argc, char *argv[]) {
     }
 
     ifstream myfile (filename);
-    string mystring;
-    char key[crypto_aead_aes256gcm_KEYBYTES];
+    unsigned char key[crypto_aead_aes256gcm_KEYBYTES];
+    crypto_aead_aes256gcm_keygen(key);
 
     if(myfile.is_open()){
         int i = 0;
@@ -62,27 +62,21 @@ int main(int argc, char *argv[]) {
         printf("Couldn't open file: \"%s\".\n", filename.c_str());
     }
     myfile.close();
-    const unsigned char * KEY = (const unsigned char *) key;
 
     char *message_buffer = NULL;
     size_t len = 0;
     size_t MESSAGE_LEN = 0;
     MESSAGE_LEN = getline(&message_buffer, &len, stdin) - 1;
-   // printf("You entered %s, which has %zu chars.\n", message_buffer, line_size -1);
     
     const unsigned char * MESSAGE = (const unsigned char*) message_buffer;
-    cout << "MESSAGE START:" << MESSAGE << ":MESSAGE END" << MESSAGE_LEN <<endl;
+    cout << "MESSAGE START:" << MESSAGE << ":MESSAGE END " << MESSAGE_LEN <<endl;
 
     int r = sodium_init();
     if (crypto_aead_aes256gcm_is_available() == 0) {
         abort(); 
     }
 
-    printf("key: ");
-    for (char c : key){
-        printf("%c",c);
-    }
-    printf("\n");
+    printf("key: %s\n", key);
 
     unsigned char nonce[crypto_aead_aes256gcm_NPUBBYTES];
    // randombytes_buf(nonce, sizeof nonce); 
@@ -98,62 +92,46 @@ int main(int argc, char *argv[]) {
         crypto_aead_aes256gcm_encrypt(ciphertext, &ciphertext_len,
                                     MESSAGE, MESSAGE_LEN,
                                     ADDITIONAL_DATA, ADDITIONAL_DATA_LEN,
-                                    NULL, nonce, KEY);
-        printf("bin len: %lld\n", ciphertext_len);
-        printf("bin: ");
-        for (auto&& c : ciphertext){
-            printf("%c",c);
-        }
-        printf("\n");
+                                    NULL, nonce, key);
+     
+        printf("bin: %s\n", ciphertext);
 
         int b64_len = sodium_base64_ENCODED_LEN(ciphertext_len, sodium_base64_VARIANT_ORIGINAL); 
         char b64[b64_len];
         sodium_bin2base64(b64, b64_len,
                             ciphertext, ciphertext_len,
                             sodium_base64_VARIANT_ORIGINAL);
-        printf("b64: ");
-        for (auto&& c : b64){
-            printf("%c",c);
-        }
-        printf("\n");
+      
+        printf("b64: %s\n", b64);
     } else if (processingType == -1 ){
         printf("decoding...\n");
         size_t bin_maxlen = ((MESSAGE_LEN*10) / 4 * 3)/10;
-        size_t* bin_len = 0;
+        size_t* bin_len;
         unsigned char bin[bin_maxlen];
         int bres = 100;
         bres = sodium_base642bin(bin, bin_maxlen,
-                    message_buffer, MESSAGE_LEN,
+                    (const char*) MESSAGE, MESSAGE_LEN,
                     NULL, bin_len,
                     NULL, sodium_base64_VARIANT_ORIGINAL);
         printf("b642bin res:%d\n",bres);
-        printf("bin: ");
-        for (auto&& c : bin){
-            printf("%c",c);
-        }
-        printf("\n");
-    
-        unsigned char decrypted[MESSAGE_LEN];
+        printf("bin: %s\n", bin);
+        unsigned char decrypted[bin_maxlen - crypto_aead_aes256gcm_ABYTES];
         unsigned long long decrypted_len;
         int res = 100;
-        if (MESSAGE_LEN < crypto_aead_aes256gcm_ABYTES ||
+        if (bin_maxlen < crypto_aead_aes256gcm_ABYTES ||
             (res = crypto_aead_aes256gcm_decrypt(decrypted, &decrypted_len,
                                         NULL,
-                                        bin, (long long unsigned int) bin_len,
+                                        bin, bin_maxlen, 
                                         ADDITIONAL_DATA,
                                         ADDITIONAL_DATA_LEN,
-                                        nonce, KEY)) != 0) {
+                                        nonce, key)) != 0) {
         }
         printf("decrypt res: %d\n", res);
 
-        printf("decrypted: ");
-        for (auto&& c : decrypted){
-            printf("%c",c);
-        }
-        printf("\n");
+        printf("decrypted: %s\n", decrypted);
     }
     return 0;
 }
-// gcc msgcrypt.cpp -lstdc++ -lsodium -o msgcrypt
+//gcc msgcrypt.cpp -lstdc++ -lsodium -o msgcrypt
 //CFLAGS=$(pkg-config --cflags libsodium)
 //LDFLAGS=$(pkg-config --libs libsodium)
